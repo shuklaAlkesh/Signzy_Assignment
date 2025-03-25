1700b72c404b1ed184866df7f0ebaab183bfa849
Alkesh shukla <shukla2002alkesh@gmail.com>
2025-03-25 09:11:42 +0530

    IMPORT CONFIG UPDATED

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "../context/AuthContext";
+import BASE_URL from "../config";
 
 function Profile() {
   const { user } = useAuth();
